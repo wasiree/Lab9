77647c52a5f56e5a9eead72ebd4a017c35041c50
Vasiree Nuntajan <wasiree098@gmail.com>
2022-12-23 17:10:45 +0700

    lab9_3 complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,44 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int age;
+    long int bounty;
+    string chr;
+    float tall;
+    cout << "Enter your age: ";
+	cin >> age;
+	   if (age <= 25){
+	      cout << "Enter your height: ";
+	      cin >> tall;
+	      if(tall < 100){
+	         chr = "Chopper";
+	         }
+	    else if(tall < 180){
+	         chr = "Usopp";
+	   }
+	   else{
+	      cout << "Enter your bounty: ";
+	      cin >> bounty;
+	     if(bounty > 1100000000){
+	        chr = "Zoro";
+	  }else{
+	        chr = "Sanji";
+}}}        
+	         
+else if(age <= 60){
+    cout << "Enter your bounty: ";
+    cin >> bounty;
+       if(bounty > 500000000){
+       chr = "Jinbe";
+     }else{
+       chr = "Franky";}
+    }
+else{
+        chr = "Brook";
+    }
+    cout << "Your character = " << chr;
+    return 0;
+}
